50b4187ad92ef94db81b16e37fe1078b8ae0f9f4
AchmadSu <ecepentis@gmail.com>
2022-10-21 16:05:31 +0700

    Upgrade fitur

--- app/Http/Controllers/API/VerificationCodesController.php
+++ app/Http/Controllers/API/VerificationCodesController.php
@@ -155,7 +155,7 @@ class VerificationCodesController extends BaseController
             $twilio = new Client($sid, $token);
             $text   = "ERAKSA\nAssets Management System\n\nKode OTP anda: *$otp*.\n\nKode ini hanya akan berlaku dalam 10 menit ke depan. Jangan bagikan kode ini kepada siapapun!$tokenMsg"; 
             $message = $twilio->messages 
-                            ->create("whatsapp:$recipient", // to 
+                        ->create("whatsapp:$recipient",
                                     array( 
                                         "from" => "whatsapp:".getenv("TWILIO_NUMBER"),       
                                         "body" => "$text",
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UsersController;
+use App\Http\Controllers\API\VerificationCodesController;
+use App\Models\VerificationCodes;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,23 +19,35 @@ use App\Http\Controllers\API\UsersController;
 
 /**--- Users --- */
 Route::controller(UsersController::class)->group(function(){
+    Route::get('users/getAll', 'index');
+    Route::get('users/getAllSuperAdmin', 'getSuperAdmin');
+    Route::get('users/detail/{id}', 'show');
     Route::post('register', 'register'); 
     Route::post('login', 'login');
+    Route::put('users/update', 'update');
+    Route::delete('users/{id}', 'delete');
+    Route::post('logout', 'logout');
+
+    Route::get('users/trash', 'trash');
+    Route::put('users/restore/{id}', 'restore');
 });
 
-/** --- Generate OTP What's App --- */
-
-Route::controller(AuthOtpController::class)->group(function(){
-    Route::post('checkOtp/{user_id}', 'checkOtp');
-    Route::post('/generate/{user_id}', 'generate')->name('generateNewOtp');
-    Route::post('/regenerate/{user_id}', 'updateOtp')->name('regenerateOtp');
+/** --- Verification Codes --- */
+Route::controller(VerificationCodesController::class)->group(function(){
+    Route::post('verification/check/{user_id}', 'checkOtp');
+    Route::post('verification/generate/{user_id}', 'generate')->name('generateNewOtp');
+    Route::post('verification/regenerate/{user_id}', 'regenerate')->name('regenerateOtp');
 });
 
-Route::get('sendSMS', [App\Http\Controllers\API\TwilioSMSController::class, 'index']);
+// Route::get('sendSMS', [App\Http\Controllers\API\TwilioSMSController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function(){
-    Route::resource('products', ProductController::class);
+    // Route::resource('products', ProductController::class);
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UsersController::class);
+    // Route::resource()
 });
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
