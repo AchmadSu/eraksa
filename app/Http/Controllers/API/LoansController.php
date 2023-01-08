2ed58e3b8cd890d1c6909e35fd01f708da740dbb
AchmadSu <ecepentis@gmail.com>
2023-01-08 13:17:46 +0700

    Configure Loans Controller

--- app/Http/Controllers/API/LoansController.php
+++ app/Http/Controllers/API/LoansController.php
@@ -23,16 +23,73 @@ class LoansController extends BaseController
     /** 
      * Get All loans
      * 
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
     */
 
-    public function index(){
+    public function index(Request $request){
         try {
             sleep(5);
+            $code = $request->code;
+            $loaner_keyWords = $request->loaner_keyWords;
+            $lender_id = $request->lender_id;
+            $dateOne = $request->dateOne;
+            $dateTwo = $request->dateTwo;
+            $status = $request->status;
+            $return_code = $request->return_code;
+            $dueDateOne = $request->dueDateOne;
+            $dueDateTwo = $request->dueDateTwo;
+
+            $from = date($dateOne);
+            $to = date($dateTwo);
+            
+            $dueFrom = date($dueDateOne);
+            $dueTo = date($dueDateTwo);
+            // dd($request->loaner_ids);
+            // \DB::enableQueryLog();
+            if($request->loaner_ids == NULL){
+                $loaner = User::
+                where('name', 'like', '%'.$loaner_keyWords.'%')
+                ->orWhere('email', 'like', '%'.$loaner_keyWords.'%')
+                ->get();
+                $loaner_ids = array();
+                foreach ($loaner as $rowLoaner) {
+                    $loaner_ids[] = $rowLoaner->id;
+                }
+            } else {
+                $loaner_ids = $request->loaner_ids;
+            }
+            // dd(isset($loaner_ids));
+            $return = Returns::where('code', 'like', '%'.$return_code.'%')->get();
+            // $loans = Loans::whereIn('loaner_id', $loaner_ids)->get();
+            $return_ids = array();
+            foreach ($return as $rowReturn) {
+                $return_ids[] = $rowReturn->id;
+            }
+            // \DB::enableQueryLog();
+            // dd($request->loaner_ids == NULL);
+            $loans = Loans::when(isset($code))
+            ->where('code', 'like', '%'.$code.'%')
+            ->when(isset($loaner_ids))
+            ->whereIn('loaner_id', $loaner_ids)
+            ->when(isset($lender_id))
+            ->where('lender_id', $lender_id)
+            ->when(isset($dateOne) && !isset($dateTwo))
+            ->where('date', $from)
+            ->when(isset($dateOne) && isset($dateTwo))
+            ->whereBetween('date', [$from, $to])
+            ->when(isset($dueDateOne) && !isset($dueDateTwo))
+            ->where('due_date', $dueFrom)
+            ->when(isset($dueDateOne) && isset($dueDateTwo))
+            ->whereBetween('due_date', [$dueFrom, $dueTo])
+            ->when(isset($status))
+            ->where('status', $status)
+            ->when(isset($return_code))
+            ->whereIn('return_id', $return_ids)
+            ->get();
             // dd(Loans::all());
             // dd(Auth::user()->name);
             // \DB::enableQueryLog();
-            $loans = Loans::all();
             // dd(\DB::getQueryLog());
             // dd($loans);
             if ($loans->isEmpty()) {
@@ -45,84 +102,26 @@ class LoansController extends BaseController
     }
 
     /** 
-     * Get All loans by filter
+     * Get Loans By Id
      * 
+     * @param Int $id
      * @return \Illuminate\Http\Response
     */
 
-    public function filterLoans(Request $request){
+    public function show(Int $id)
+    {
         try {
-            // dd(Loans::all());
             sleep(5);
-            $code = $request->code;
-            
-            $loaner_name = $request->loaner_name;
-            $lender_id = $request->lender_id;
-            $dateOne = $request->dateOne;
-            $dateTwo = $request->dateTwo;
-            $status = $request->status;
-            $return_code = $request->return_code;
-            $dueDateOne = $request->dueDateOne;
-            $dueDateTwo = $request->dueDateTwo;
-
-            
-            $from = date($dateOne);
-            $to = date($dateTwo);
-            
-            $dueFrom = date($dueDateOne);
-            $dueTo = date($dueDateTwo);
-            // dd($request->loaner_ids);
-            // \DB::enableQueryLog();
-            if($request->loaner_ids == NULL){
-                if($loaner_name != NULL) {
-                    $loaner = User::where('name', 'like', '%'.$loaner_name.'%')->get();
-                    // dd(\DB::getQueryLog());
-                    $loaner_ids = array();
-                    foreach ($loaner as $rowLoaner) {
-                        $loaner_ids[] = $rowLoaner->id;
-                    }
-                }
-            } else {
-                $loaner_ids = $request->loaner_ids;
-            }
-            // dd($loaner_ids);
-            $return = Returns::where('code', 'like', '%'.$return_code.'%')->get();
-            // $loans = Loans::whereIn('loaner_id', $loaner_ids)->get();
-            $return_ids = array();
-            foreach ($return as $rowReturn) {
-                $return_ids[] = $rowReturn->id;
-            }
-            // \DB::enableQueryLog();
-            // dd($loaner_ids);
-            $loans = Loans::
-                    when(isset($code))
-                    ->where('code', 'like', '%'.$code.'%')
-                    ->when(isset($loaner_ids))
-                    ->whereIn('loaner_id', ($loaner_ids))
-                    ->when(isset($lender_id))
-                    ->where('lender_id', $lender_id)
-                    ->when(isset($dateOne) && !isset($dateTwo))
-                    ->where('date', $from)
-                    ->when(isset($dateOne) && isset($dateTwo))
-                    ->whereBetween('date', [$from, $to])
-                    ->when(isset($dueDateOne) && !isset($dueDateTwo))
-                    ->where('due_date', $dueFrom)
-                    ->when(isset($dueDateOne) && isset($dueDateTwo))
-                    ->whereBetween('due_date', [$dueFrom, $dueTo])
-                    ->when(isset($status))
-                    ->where('status', $status)
-                    ->when(isset($return_code))
-                    ->whereIn('return_id', $return_ids)
-                    ->get();
-            // dd(Auth::user());
+            $Loans = Loans::where('id', $id)->first();
             // dd(\DB::getQueryLog());
-            // var_dump(Loans::all());exit();
-            if ($loans->isEmpty()) {
+            if (!$Loans) {
                 return $this->sendError('Error!', ['error' => 'Data tidak ditemukan!']);
             }
-            return $this->sendResponse($loans, 'Displaying all Loans data');
+            return $this->sendResponse($Loans, 'Loans detail by Id');
         } catch (\Throwable $th) {
             return $this->sendError('Error!', ['error' => $th]);
         }
+        
     }
+    
 }
